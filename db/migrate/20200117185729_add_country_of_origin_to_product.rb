ed02d8037899cd75f4b7ca04c7bdf5be99a3361e
Student <example@example.com>
2020-01-17 11:06:50 -0800

    set migrations

---
+++ db/migrate/20200117185729_add_country_of_origin_to_product.rb
@@ -0,0 +1,5 @@
+class AddCountryOfOriginToProduct < ActiveRecord::Migration[5.2]
+  def change
+        add_column(:products, :country_of_origin, :string)
+  end
+end
---
+++ db/migrate/20200117185854_add_content_body_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddContentBodyToReviews < ActiveRecord::Migration[5.2]
+  def change
+    add_column(:reviews, :content, :string)
+  end
+end
