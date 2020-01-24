b03cf51e3007539578fc4c66582c2a399bccc601
Student <example@example.com>
2020-01-24 15:21:20 -0800

    add layouts and views

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+
+  before_save(:titleize_project)
+
+private
+  def titleize_project
+    self.name = self.name.titleize
+  end
 end
