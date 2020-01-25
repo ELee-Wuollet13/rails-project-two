6dd6ef16bb01947c713c368c76ad813c7fb34bd6
Student <example@example.com>
2020-01-24 17:42:58 -0800

    fix params so that products save with values

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,52 +1,55 @@
 class ProductsController < ApplicationController
-    def index
-      @products = Product.all
-      render :index
-    end
 
-    def new
-      @product = Product.new
+  def index
+    @products = Product.all
+    render :index
+  end
+
+  def new
+    @product = Product.new
+    render :new
+  end
+
+  def create
+    @album = Album.find(params[:album_id])
+    @song = @album.songs.new(song_params)
+    if @song.save
+      redirect_to album_path(@album)
+    else
       render :new
     end
+  end
 
-    def create
-      @product = Product.new(product_params)
-      if @product.save
-        flash[:notice] = "You've successfully added and item!"
-        # session[:product_id] = @product.id
-        redirect_to new_product_path
-      else
-        flash[:alert] = "Sorry, that item didn't load."
-        redirect_to signup_path
-      end
-    end
 
-    def edit
-      @product = Product.find(params[:id])
+  def edit
+    @album = Album.find(params[:album_id])
+    @song = Song.find(params[:id])
+    render :edit
+  end
+  
+  def show
+    @album = Album.find(params[:album_id])
+    @song = Song.find(params[:id])
+    render :show
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to album_path(@song.album)
+    else
       render :edit
     end
+  end
 
-    def show
-      @review = Review.find(params[:id])
-      render :show
-    end
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to album_path(@song.album)
+  end
 
-    def update
-      @review= Review.find(params[:id])
-      if @review.update(review_params)
-        redirect_to reviews_path
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @review = Review.find(params[:id])
-      @review.destroy
-      redirect_to reviews_path
-    end
   private
-  def product_params
-    params.require(:product).permit(:cost, :reviews, :reviews, :country_of_origin)
+  def song_params
+    params.require(:song).permit(:name, :lyrics)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,14 +2,14 @@ class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
 
-  before_save(:titleize_project)
-
-  private
-  def titleize_project
-    # self.name = self.name.titleize
-
-    self.name = self.name.try(:titleize)
-
-
-  end
+  # before_save(:titleize_project)
+  #
+  # private
+  # def titleize_project
+  #   # self.name = self.name.titleize
+  #
+  #   self.name = self.name.try(:titleize)
+  #
+  #
+  # end
 end
